bdb3854859d09bc10459740d87c559d9f317104d
ilias-himi-vinci <ilias.himi@student.vinci.be>
2025-10-02 14:45:05 +0200

    new: ex1.2

---
+++ 1.2/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+
+import filmRouter from "./routes/films";
+import { requestCounterMiddleware } from "./utils/counter";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let requestCount = 0;
+app.use((req, _res, next) => {
+  if (req.method === "GET") {
+    requestCount++;
+    console.log(`GET counter : ${requestCount}`);
+  }
+  next();
+});
+
+
+/* Challenge of ex1.2 */
+app.use(requestCounterMiddleware);
+
+app.use("/films", filmRouter);
+
+export default app;
